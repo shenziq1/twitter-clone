3cf9e669ea701e7863ebb18dbda0a53fcd311312
Ziqi Shen <ziqi.shen@mail.utoronto.ca>
2021-04-04 12:06:17 -0400

    modifies app to use flask_restful

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify, render_template, flash, redirect
+from flask import Flask, request, jsonify, render_template, flash, redirect, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_restful import Resource
+from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
@@ -39,45 +39,45 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
-@login_required
-@app.route('/')
-def index():
-    if current_user.is_authenticated:
-        return "Hello, " + current_user.get_username() + " !"
-    else:
-        return "Hello, please register or login!"
+class IndexApi(Resource):
+    def get(self):
+        if current_user.is_authenticated:
+            return "Hello, " + current_user.get_username() + " !"
+        else:
+            return "Hello, please register or login!"
 
-@app.route('/users')
-def users():
-    users = User.query.all()
-    return jsonify([{"username": i.username, "password_hash": i.password_hash, "id": i.id} for i in users])
+class UsersApi(Resource):
+    def get(self):
+        users = User.query.all()
+        return jsonify([{"username": i.username, "password_hash": i.password_hash, "id": i.id} for i in users])
 
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if current_user.is_authenticated:
-        return redirect('/')
-    if request.method == 'POST':
+class RegisterApi(Resource):
 
+    def get(self):
+        if current_user.is_authenticated:
+            return redirect('/')
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('register.html'), 200, headers)
+
+    def post(self):
         try:
             username = request.form['username']
             password = request.form['password']
             password2 = request.form['password2']
 
             if not (username and password and password2):
-                raise InvalidParameter
+                return('Fields required.')
 
             else:
-
                 if password != password2:
-                    raise Exception('Password mismatch!')
+                    return('Password mismatch!')
 
                 if User.query.filter_by(username=username).first():
-                    raise Exception('User exists, try another username!')
+                    return('User exists, try another username!')
 
                 user = User(username=username, password=password)
                 db.session.add(user)
@@ -86,26 +86,36 @@ def register():
         except Exception as e:
             return jsonify({"error": e})
 
-    return render_template('register.html')
 
-@app.route('/login', methods = ['POST', 'GET'])
-def login():
-    if current_user.is_authenticated:
-        return redirect('/')
+class LoginApi(Resource):
+    def get(self):
+        if current_user.is_authenticated:
+            return redirect('/')
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('login.html'), 200, headers)
 
-    if request.method == 'POST':
+    def post(self):
         username = request.form['username']
         user = User.query.filter_by(username = username).first()
         if user is not None and user.check_password(request.form['password']):
             login_user(user)
             return redirect('/')
 
-    return render_template('login.html')
+class LogoutApi(Resource):
+    def get(self):
+        logout_user()
+        return redirect('/')
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect('/')
+def initialize_routes(api):
+    api.add_resource(UsersApi, '/users')
+    api.add_resource(IndexApi, '/')
+    api.add_resource(RegisterApi, '/register')
+    api.add_resource(LoginApi, '/login')
+    api.add_resource(LogoutApi, '/logout')
+
+
+api = Api(app)
+initialize_routes(api)
 
 if __name__ == "__main__":
     app.run(debug=True)
